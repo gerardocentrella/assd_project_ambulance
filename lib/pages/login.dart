8c3461319a1c892216609c89eaec7c4f50c9a224
Gerardo Centrella <centrella.gerardo0@gmail.com>
2024-08-17 12:01:09 +0200

    added login page. updated main with login page. updated work file

---
+++ lib/pages/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget{
+  const Login({super.key});
+
+  @override
+  _Login createState(){
+    return _Login();
+  }
+}
+
+class _Login extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightGreen,
+    );
+  }
+}
