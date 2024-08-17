78cfdb9d207844c264c233ecc33f0a94b8d5b76a
RaffaeleMasucci <80713517+RaffaeleMasucci@users.noreply.github.com>
2024-08-17 17:23:09 +0200

    login commit

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -19,7 +19,22 @@ class _Login extends State<Login> {
 
     return Scaffold(
       backgroundColor: Colors.redAccent,
-      appBar: AppBar(title: const Text('LogIn'), centerTitle: true, backgroundColor: Colors.white, elevation: 100),
+      appBar: AppBar(title: const Text('My App'),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 100,
+          toolbarHeight: 50,
+          titleTextStyle: const TextStyle(
+            color: Colors.indigoAccent,
+            fontSize: 30,
+            fontStyle: FontStyle.normal),
+          shadowColor: Colors.lightGreenAccent,
+          shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: const BorderSide(color: Colors.transparent, width: 5),
+            gapPadding: 120,)
+          )
+
     );
   }
 }
