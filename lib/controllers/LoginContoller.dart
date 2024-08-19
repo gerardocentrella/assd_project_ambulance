432c53c2f4aa60aa099ac0cd6e9de68138cb73ea
RaffaeleMasucci <80713517+RaffaeleMasucci@users.noreply.github.com>
2024-08-19 17:05:03 +0200

    login commit

---
+++ lib/controllers/LoginContoller.dart
@@ -0,0 +1,18 @@
+class LoginController {
+
+  late int _code;
+
+  bool login(String ambulanceId, String password) {
+
+    // invocazione metodo specifico di login del servizio RESTFul
+    if(_code == 200) {
+      // successo --> LOGIN
+
+      return true;
+    } else {
+      // errore
+      return false;
+    }
+
+  }
+}
