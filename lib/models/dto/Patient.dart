d6f03e41018fbfdbd0998961b9c5fef568c44710
Gerardo Centrella <centrella.gerardo0@gmail.com>
2024-08-19 15:46:15 +0200

    updated appbar, home, main

--- lib/models/dto/Patient.dart
+++ lib/models/dto/Patient.dart
@@ -1,17 +1,53 @@
-class PatientDTO {
+class Patient {
 
-  String name;
-  String surname;
-  String city;
-  String address;
-  int age;
+  String _name;
+  String _surname;
+  String _city;
+  String _address;
+  int _age;
 
-  PatientDTO({
-    required this.name,
-    required this.surname,
-    required this. city,
-    required this.address,
-    required this.age
-  });
-  
+  Patient({required String name, required String surname,
+    required String city, required String address, required int age})
+  : this._name = name,
+    this._surname = surname,
+    this._city = city,
+    this._address = address,
+    this._age = age;
+
+  String get name => _name;
+
+  set name(String value) {
+    _name = value;
+  }
+
+  String get surname => _surname;
+
+  int get age => _age;
+
+  set age(int value) {
+    _age = value;
+  }
+
+  String get address => _address;
+
+  set address(String value) {
+    _address = value;
+  }
+
+  String get city => _city;
+
+  set city(String value) {
+    _city = value;
+  }
+
+  set surname(String value) {
+    _surname = value;
+  }
+
+  @override
+  String toString() {
+    return 'Patient{_name: $_name, _surname: $_surname, _city: $_city, _address: $_address, _age: $_age}';
+  }
+
+
 }
--- lib/models/dto/Position.dart
+++ lib/models/dto/Position.dart
@@ -1,8 +1,27 @@
-class PositionDTO {
+class Position {
 
-  double latitude;
-  double longitude;
+ double _latitude;
+ double _longitude;
 
-  PositionDTO({required this.latitude, required this.longitude});
+  Position({required double latitude, required double longitude})
+  : this._latitude = latitude,
+    this._longitude = longitude;
+
+ double get longitude => _longitude;
+
+  set longitude(double value) {
+    _longitude = value;
+  }
+
+  double get latitude => _latitude;
+
+  set latitude(double value) {
+    _latitude = value;
+  }
+
+ @override
+  String toString() {
+    return 'Position{_latitude: $_latitude, _longitude: $_longitude}';
+  }
 
 }
