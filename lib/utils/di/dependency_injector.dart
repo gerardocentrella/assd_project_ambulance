255277a9604d6036189b06e49618e265cb2b3276
Gerardo Centrella <centrella.gerardo0@gmail.com>
2024-08-27 17:10:42 +0200

    commit

--- lib/utils/di/dependency_injector.dart
+++ lib/utils/di/dependency_injector.dart
@@ -7,8 +7,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 import '../../controllers/auth/bloc/auth_bloc.dart';
+import '../../controllers/emergency_room_reached_controller.dart';
 import '../../controllers/patient_controller.dart';
 import '../../controllers/patient_reached/patient_reached_bloc.dart';
+import '../../controllers/services/emergency_room_reached_service.dart';
+import '../../controllers/services/patient_reached_service.dart';
 import '../../models/repository/authentication_repository.dart';
 import '../../models/repository/token_repository.dart';
 
@@ -18,9 +21,29 @@ class DependencyInjector extends StatelessWidget {
   const DependencyInjector({super.key, required this.child});
 
   @override
-  Widget build(BuildContext context) =>
-      _repositories(child: _block(child: child));
+  Widget build(BuildContext context) {
+    return _repositories(
+      child: _controllers(
+        child: _block(child: child),
+      ),
+    );
+  }
 
+  Widget _controllers({required Widget child}) => MultiProvider(
+    providers: [
+      Provider<PatientReachedService>(
+          create: (context) => PatientReachedService()),
+      ProxyProvider<PatientReachedService, PatientReachedController>(
+          update: (context, service, previous) =>
+              PatientReachedController(service)),
+      Provider<EmergencyRoomReachedService>(
+          create: (context) => EmergencyRoomReachedService()),
+      ProxyProvider<EmergencyRoomReachedService, EmergencyRoomReachedController>(
+          update: (context, service, previous) =>
+              EmergencyRoomReachedController(service)),
+    ],
+    child: child,
+  );
   /*
   Widget _providers({required Widget child}) => MultiProvider(
     providers:const [
