04bd764d3613468fd7cb238fe32d081982842bcc
Ran-shiks <93742851+Ran-shiks@users.noreply.github.com>
2024-09-02 15:51:57 +0200

    Miglioramento leggibilità del codice

--- lib/pages/myapp.dart
+++ lib/pages/myapp.dart
@@ -16,68 +16,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _AppViewState extends State<MyApp> {
-  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+  final _navigatorKey = GlobalKey<NavigatorState>();
 
   NavigatorState get _navigator => _navigatorKey.currentState!;
 
   @override
-  Widget build(BuildContext context) {
-    return DependencyInjector(
+  Widget build(BuildContext context) => DependencyInjector(
       child: MaterialApp(
+        routes: {
+          '/patientreached': (context) => const PatientReachedForm(),
+        },
         navigatorKey: _navigatorKey,
-        onGenerateRoute: _onGenerateRoute,
-        builder: (context, child) => AuthenticationListener(
-          navigator: _navigator,
-          child: child,
-        ),
-      ),
-    );
-  }
-
-  Route<dynamic> _onGenerateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/patientreached':
-        return MaterialPageRoute(
-          builder: (_) => const PatientReachedForm(),
-        );
-      default:
-        return SplashPage.route();
-    }
-  }
-}
-
-class AuthenticationListener extends StatelessWidget {
-  final Widget? child;
-  final NavigatorState navigator;
-
-  const AuthenticationListener({
-    required this.navigator,
-    this.child,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocListener<AuthenticationBloc, AuthState>(
-      listener: (context, state) {
-        switch (state.status) {
-          case AuthenticationStatus.authenticated:
-            navigator.pushAndRemoveUntil<void>(
-              Home.route(),
-                  (route) => false,
-            );
-            break;
-          case AuthenticationStatus.unauthenticated:
-            navigator.pushAndRemoveUntil<void>(
-              Login.route(),
-                  (route) => false,
-            );
-            break;
-          case AuthenticationStatus.unknown:
-            break;
-        }
-      },
-      child: child,
-    );
-  }
+        builder: (context, child) {
+          return BlocListener<AuthenticationBloc, AuthState>(
+            listener: (context, state) {
+              switch (state.status) {
+                case AuthenticationStatus.authenticated:
+                  _navigator.pushAndRemoveUntil<void>(
+                    Home.route(),
+                        (route) => false,
+                  );
+                case AuthenticationStatus.unauthenticated:
+                  _navigator.pushAndRemoveUntil<void>(
+                    Login.route(),
+                        (route) => false,
+                  );
+                case AuthenticationStatus.unknown:
+                  break;
+              }
+            },
+            child: child,
+          );
+        },
+        onGenerateRoute: (_) => SplashPage.route(),
+      ));
 }
